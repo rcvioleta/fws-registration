a39a805fbacc6f19ec94bef1f4b9d279eac914f3
Rogene Cris Violeta <rogenecris.violeta@flatworld.com.ph>
2019-04-24 07:03:24 +0800

    Redirect index and register routes to login

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,16 @@
 */
 
 Route::get('/', function () {
-  return view('welcome');
+  return view('auth.login');
+});
+
+Route::get('/register', function () {
+  return view('auth.login');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/register', function () {
-  return view('auth.login');
-});
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
   Route::resource('/employee', 'EmployeeController');
